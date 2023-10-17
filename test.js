0029957e072a2481f64ed008b61d0127d6edb4ba
wikigithub <yunming.lang@gmail.com>
2023-10-17 13:59:15 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,19 +1,31 @@
 $request.url.replace('https', 'http');
 if ($network.wifi.ssid === 'Matrix' && $request.headers['Host'].includes('39561de9e2cd43229ba638cd0b39f817') && $request.url.includes("/library/parts/") && $request.url.includes(".mkv")) 
 {
-$done({
-status: 307,
-headers: {
-'Location': $request.url.replace('https://10-0-0-199.39561de9e2cd43229ba638cd0b39f817.plex.direct:32400', 'http://192.168.5.6:9090').replace('mkv','mkv?vhost=plex.movie')
-}
-})
+
+    var strReq = request.url
+    var iStart = strReq.indexOf("https://")
+    var iEnd = strReq.indexOf(".plex.direct:32400")
+    console.log(iStart.toString())
+    var strEnd= iEnd.toString()
+    console.log(strEnd)
+    var strOldAddr = "https://" + strReq.slice(iStart + "https://".length,iEnd + ".plex.direct:32400".length)
+    console.log(strOldAddr)
+    var strNewAddr = strReq.replace(strOldAddr,"http://192.168.5.6:9090")
+    console.log(strNewAddr)
   
-$response.status = 307;
-let respheaders = $response.headers;
-respheaders['Content-Length'] = '0';
-
-
-$done({respheaders});
+    $done({
+    status: 307,
+    headers: {
+    'Location': strNewAddr
+    }
+    })
+      
+    $response.status = 307;
+    let respheaders = $response.headers;
+    respheaders['Content-Length'] = '0';
+    
+    
+    $done({respheaders});
 }
 //console.log($request.headers['Host']);
 
