bdec74c812097a53b75e9b70a598902e8a25042e
wikigithub <yunming.lang@gmail.com>
2023-10-17 11:41:30 +0800

    Update test.js

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ $request.url.replace('https', 'http');
 if ($request.headers['Host'].includes('39561de9e2cd43229ba638cd0b39f817') && $request.url.includes("/library/parts/") && $request.url.includes(".mkv")) 
 {
 $done({
-status: 307,
+status: 302,
 headers: {
 'Location': $request.url.replace('https://10-0-0-199.39561de9e2cd43229ba638cd0b39f817.plex.direct:32400', 'http://192.168.5.6:9090').replace('mkv','mkv?vhost=plex.movie')
 }
@@ -10,7 +10,7 @@ headers: {
 }
 //console.log($request.headers['Host']);
 
-$response.status = 307;
+$response.status = 302;
 let respheaders = $response.headers;
 respheaders['Content-Length'] = '0';
 
