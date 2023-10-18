723b19350bb349c4991f50bd6488a60773320f28
wikigithub <yunming.lang@gmail.com>
2023-10-18 13:53:15 +0800

    Update location.js

--- location.js
+++ location.js
@@ -1,2 +1,11 @@
-$request.url = $request.url.replace('123eoe.com', '163.com');
-$done({$request});
+//$request.url = $request.url.replace('123eoe.com', '163.com');
+//$done({$request});
+
+if ($request.url.includes('123eoe.com')) {
+$done({
+status: 302,
+headers: {
+'Location': $request.url.replace('123eoe.com', '163.com')
+}
+})
+}
