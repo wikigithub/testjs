02cd3e064ab77e5dd1b55a90cb9f48cfa2038950
wikigithub <yunming.lang@gmail.com>
2023-10-20 16:50:08 +0800

    Update lc3.js

--- lc3.js
+++ lc3.js
@@ -2,16 +2,14 @@ let strAddr = $request.url;
 let headers = $request.headers;
 headers['test'] = 'test';
 //$done({strAddr});
-console.log("testbbb");
+console.log("testbbb1");
 var response = new Object();
 response.status = 302;
+response.headers = {
+    'Location': '192.168.5.6'
+    };
 console.log(response.status);
 
 
 
-$done({
-    status: 302,
-    headers: {
-    'Location': '192.168.5.6'
-    }
-    });
+$done({response});
