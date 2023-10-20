ac1da151de5a9a03a6bc011a9f03ee0b59247d6d
wikigithub <yunming.lang@gmail.com>
2023-10-20 16:18:15 +0800

    Update lc2.js

--- lc2.js
+++ lc2.js
@@ -2,5 +2,14 @@ let strAddr = $request.url;
 let headers = $request.headers;
 headers['test'] = 'test';
 //$done({strAddr});
-console.log("test33");
-$done({headers});
+console.log("test333");
+/*vat response
+response.status = 302;
+response.headers = "";*/
+//$done({headers});
+$done({
+    status: 302,
+    headers: {
+    'Location': '192.168.5.6'
+    }
+    });
