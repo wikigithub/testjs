afa66b15060f022f9ec6584c7d41bba48e43183c
wikigithub <yunming.lang@gmail.com>
2023-10-20 17:18:48 +0800

    Create relocplex.js

---
+++ relocplex.js
@@ -0,0 +1,63 @@
+$request.url.replace('https', 'http');
+//if ($network.wifi.ssid === 'Matrix' && $request.headers['Host'].includes('39561de9e2cd43229ba638cd0b39f817') && $request.url.includes("/library/parts/") && $request.url.includes(".mkv")) 
+if ( ($request.headers['Host'].includes('39561de9e2cd43229ba638cd0b39f817') || $request.headers['Host'].includes('d39aae8764674d02a191e33b610246fd'))
+    && $request.url.includes("/library/parts/")
+    && $request.url.includes(".mkv")) 
+{
+
+    var strVhost = "plex.movie"
+    if( $request.headers['Host'].includes('d39aae8764674d02a191e33b610246fd'))
+    {
+        strVhost = "plex.tv"
+    }
+
+    /*const Http = new XMLHttpRequest();
+    const url = 'http://plex.123eoe.com:32400/library/parts/61002/1639618033/file.mkv'
+
+    Http.open("GET", url);
+    Http.setRequestHeader('X-Plex-Token', 'JEttnZi-5wovg_QJUwFx');
+    Http.send();
+   
+    Http.onreadystatechange = (e) => {
+      console.log(Http.responseText)
+    }*/
+
+    var strReq = $request.url
+    var iStart = strReq.indexOf("https://")
+    var iEnd = strReq.indexOf(".plex.direct:32400")
+    console.log(iStart.toString())
+    var strEnd= iEnd.toString()
+    console.log(strEnd)
+    var strOldAddr = "https://" + strReq.slice(iStart + "https://".length,iEnd + ".plex.direct:32400".length)
+    console.log(strOldAddr)
+    var strNewAddr = strReq.replace(strOldAddr,"http://192.168.104.62.6:9091")
+    console.log(strNewAddr)
+    if($request.url.includes("?"))
+    {
+        strNewAddr = strNewAddr + "&vhost=" + strVhost
+    }
+    else
+    {
+        strNewAddr = strNewAddr + "?vhost=" + strVhost
+    }
+    
+    console.log(strNewAddr)
+    
+    console.log("testbbb22");
+    var response = new Object();
+    response.status = 302;
+    response.headers = {
+        'Location': strNewAddr
+        };
+    console.log(response.status);
+    
+    
+    
+    $done({response});
+  
+}
+//console.log($request.headers['Host']);
+
+
+
+
